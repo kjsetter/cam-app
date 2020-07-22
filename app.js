aed558067d84aa80744ef76735a52d8a21bf9653
Kevin Setter <ksetter@telus.net>
2020-07-22 14:32:05 -0700

    no message

--- app.js
+++ app.js
@@ -7,15 +7,15 @@ const cameraView = document.querySelector("#camera--view"),
     cameraTrigger = document.querySelector("#camera--trigger")
 // Access the device camera and stream to cameraView
 function cameraStart() {
-    navigator.mediaDevices
-        .getUserMedia(constraints)
-        .then(function(stream) {
-        track = stream.getTracks()[0];
-        cameraView.srcObject = stream;
-    })
-    .catch(function(error) {
-        console.error("Oops. Something is broken.", error);
-    });
+    //navigator.mediaDevices
+    //    .getUserMedia(constraints)
+    //    .then(function(stream) {
+    //    track = stream.getTracks()[0];
+    //    cameraView.srcObject = stream;
+    //})
+    //.catch(function(error) {
+    //    console.error("Oops. Something is broken.", error);
+    //});
 
     document.getElementById("photo_upload").addEventListener("change",
     function(){
